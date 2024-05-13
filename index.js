abe9e90d9db06febcfba306c7af269d02027d0b8
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-05-13 15:18:02 -0500

    navigation settings

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
+
 import {AppRegistry} from 'react-native';
-import App from './App';
+import { PokedexApp } from './src/PokedexApp';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(appName, () => PokedexApp);
