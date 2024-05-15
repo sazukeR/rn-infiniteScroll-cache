6818a42295bbf32edc226fa96deb186f6907958f
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-05-15 17:32:29 -0500

    Add img pokeball component

--- src/presentation/navigator/StackNavigator.tsx
+++ src/presentation/navigator/StackNavigator.tsx
@@ -13,7 +13,10 @@ const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
       <Stack.Screen name="SearchScreen" component={SearchScreen} />
