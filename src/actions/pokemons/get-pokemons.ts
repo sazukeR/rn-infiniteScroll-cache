f5cd6ec098f59bff71aed3bd0f89576a3bb37d96
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-05-15 16:19:07 -0500

    Add pokemon interface and mapper

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,19 +1,34 @@
 import { pokeApi } from "../../config/api/pokeApi";
 import { Pokemon } from "../../domain/entities/pokemon";
+import { PokeAPIPaginatedResponse, PokeAPIPokemon } from "../../infrastructure/interfaces/pokeapi.interface";
+import { PokemonMapper } from "../../infrastructure/mappers/pokemon.mapper";
 
 
-export const getPokemons = async ():Promise<Pokemon[]> => {
+export const getPokemons = async (
+ page: number,
+ limit: number = 20,
+): Promise<Pokemon[]> => {
 
   try {
    
-    const url = '/pokemon';
-    const { data } = await pokeApi.get(url);
-    console.log(data);
-    return [];
+    const url = `/pokemon?offset=${page * 10}&limit=${limit}`;
+    const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+
+    const pokemonPromises = data.results.map( (info) => {
+      return pokeApi.get<PokeAPIPokemon>(info.url)
+    })
+
+    const pokeApiPokemons = await Promise.all(pokemonPromises);
+
+    const pokemonsPromises = pokeApiPokemons.map(item =>
+     PokemonMapper.pokeApiPokemonToEntity(item.data),
+   );
+
+
+   return await Promise.all(pokemonsPromises);
 
   } catch (error) {
-   
+    console.log(error)
     throw new Error('Error getting pokemons');
-
   };
 };
